b6fa7aeefe4c13bec321298cf85b30190755f915
Vinicius D L Melo <viniciusd@Viniciuss-MacBook-Pro.local>
2017-03-05 14:35:23 -0300

    Adds initial test_array file and removes some code from array.py

--- projeto1/array.py
+++ projeto1/array.py
@@ -8,35 +8,29 @@ class UnboundedArray(np.ndarray):
         return np.asarray(input_array).view(cls)
 
     def _generate_bounds(self, arr, item):
-        print(item)
-        print(arr)
         if len(item) > 2:
             raise NotImplementedError
 
         for axis, x in ((axis, x) for axis, x in enumerate(item) if isinstance(x, slice)):
             p, q = -x.start if x.start < 0 else 0, x.stop-1 if x.stop > 1 else 0
             if axis == 0:
-                print('axis '+str(axis)+' \n p '+str(p)+' ; q '+str(q))
-                print(np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((1, -1)))
                 if p:
                     arr = np.vstack((np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((1, -1)),
-                             arr)
-                             )
+                                     arr)
+                                    )
                 if q:
                     arr = np.vstack((arr,
-                             (np.array([[arr.mean()]*(arr.shape[axis]), ]*q).reshape((1, -1))))
-                             )
+                                     (np.array([[arr.mean()]*(arr.shape[axis]), ]*q).reshape((1, -1))))
+                                    )
             elif axis == 1:
-                print('axis '+str(axis)+' \n p '+str(p)+' ; q '+str(q))
-                print(np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((-1, 1)))
                 if p:
-                    arr = hstack((np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((-1, 1)),
-                                 arr)
-                                 ) if p else arr
+                    arr = np.hstack((np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((-1, 1)),
+                                     arr)
+                                    )
                 if q:
-                    arr = hstack((arr,
-                                 (np.array([[arr.mean()]*(arr.shape[axis]), ]*q).reshape((-1, 1))))
-                                 ) if q else arr
+                    arr = np.hstack((arr,
+                                     (np.array([[arr.mean()]*(arr.shape[axis]), ]*q).reshape((-1, 1))))
+                                    )
         return arr
 
     def __getitem__(self, item):
@@ -51,7 +45,3 @@ class UnboundedArray(np.ndarray):
 
     def __repr__(self):
         return str(np.array(self))
-
-a = UnboundedArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# print(a[0, 1])
-#print(a[-1:1, -1:1])
