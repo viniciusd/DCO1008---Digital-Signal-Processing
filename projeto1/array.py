a2054f030a45dca58579eb8e247bcd917dfe4b0f
Vinicius D L Melo <viniciusd@Viniciuss-MacBook-Pro.local>
2017-03-05 16:59:20 -0300

    Makes __getitem__ return a column/line vector instead of 1D array when slicing asks so

--- projeto1/array.py
+++ projeto1/array.py
@@ -8,12 +8,12 @@ class UnboundedArray(np.ndarray):
         return np.asarray(input_array).view(cls)
 
     def _generate_bounds(self, arr, item):
-        if len(item) > 2:
-            raise NotImplementedError
+        print(arr)
 
         for axis, x in ((axis, x) for axis, x in enumerate(item) if isinstance(x, slice)):
             p = -x.start if x.start < 0 else 0
-            q = x.stop-1 if x.stop > 1 else 0
+            q = x.stop-2 if x.stop > 2 else 0
+            # print(p,q)
             if axis == 0:
                 if p:
                     arr = np.vstack((np.array([[arr.mean()]*(arr.shape[axis]), ]*p).reshape((1, -1)),
@@ -36,10 +36,21 @@ class UnboundedArray(np.ndarray):
         return arr
 
     def __getitem__(self, item):
+        if len(item) > 2:
+            raise NotImplementedError
+
         bounds = tuple(slice(max(x.start, 0), min(x.stop, size), x.step) if isinstance(x, slice) else x for x, size in zip(item, self.shape))
-        arr = np.array(super().__getitem__(bounds))
+        arr = super().__getitem__(bounds)
+        arr = np.array(arr)
+        if arr.ndim == 1:
+            if isinstance(bounds[0], slice):
+                arr = arr.reshape((-1,1))
+            elif isinstance(bounds[1], slice):
+                arr = arr.reshape((1,-1))
+
         if bounds != item:
             arr = self._generate_bounds(arr, item)
+
         return arr
 
     def __str__(self):
--- projeto1/test_array.py
+++ projeto1/test_array.py
@@ -10,20 +10,21 @@ class TestUnboundedArray(unittest.TestCase):
     def setUp(self):
         self.arr = UnboundedArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-    def test_print_normal_element(self):
+    def test_getting_normal_element(self):
         self.assertEqual(self.arr[0, 1], 2)
 
-    def test_print_normal_element_2(self):
+    def test_getting_normal_element_2(self):
         self.assertEqual(self.arr[1, 0], 4)
 
-    def test_print_generating_horizontal_borders(self):
+    @unittest.skip('It is passing, let us skip it while not fixing the other one')
+    def test_getting_generating_horizontal_borders(self):
         np.testing.assert_array_equal(self.arr[-1:1, 0], np.array([1, 1, ]).reshape((-1,1)))
 
-    def test_print_generating_horizontal_borders_2(self):
+    def test_getting_generating_horizontal_borders_2(self):
         np.testing.assert_array_equal(self.arr[-1:2, 0], np.array([2.5, 1, 4]).reshape((-1,1)))
 
     @unittest.skip('Skipping for now')
-    def test_print_generating_vertical_borders(self):
+    def test_getting_generating_vertical_borders(self):
         self.arr[1, -1:1]
 
 if __name__ == '__main__':
