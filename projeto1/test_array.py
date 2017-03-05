30718eb086dbac295b191ca6ee7f857e9f06e84d
Vinicius D L Melo <viniciusd@Viniciuss-MacBook-Pro.local>
2017-03-05 14:44:49 -0300

    Enhances our test_array file

--- projeto1/test_array.py
+++ projeto1/test_array.py
@@ -3,6 +3,16 @@ import unittest
 
 from array import UnboundedArray
 
-a = UnboundedArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-print(a[0, 1])
-print(a[-1:1, -1:1])
+class TestUnboundedArray(unittest.TestCase):
+
+    def setUp(self):
+        self.arr = UnboundedArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+    def test_print_normal_element(self):
+        self.assertEqual(self.arr[0, 1], 2)
+
+    def test_print_generating_borders(self):
+        self.arr[-1:1, -1:1]
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
