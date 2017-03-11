11edaedde3a3f0e3fe09d3d1d898ad25909aa764
Vinícius Dantas <viniciusd@Viniciuss-MacBook-Pro.local>
2017-03-11 20:07:08 -0300

    Cleans the code a bit

--- projeto1/main.py
+++ projeto1/main.py
@@ -1,9 +1,9 @@
 #!bin/python3
-from scipy import misc
-from scipy import signal
 import scipy
 import numpy as np
 from skimage.measure import compare_ssim
+import matplotlib.pylab as plt
+
 
 from array import UnboundedArray
 
@@ -22,8 +22,8 @@ kernel1 = 1/9*np.ones((3,3))
 kernel2 = np.array([[0,1,0], [1, -4, 1], [0, 1, 0]])
 
 for padding in ('zero', 'mean'):
-    web_image = UnboundedArray(misc.imread('lena.png').astype(float), padding=padding)
-    sigaa_image = UnboundedArray(rgb2gray(misc.imread('lena.bmp', mode='RGB')).astype(float), padding=padding)
+    web_image = UnboundedArray(scipy.misc.imread('lena.png').astype(float), padding=padding)
+    sigaa_image = UnboundedArray(rgb2gray(scipy.misc.imread('lena.bmp', mode='RGB')).astype(float), padding=padding)
 
     for im, image in enumerate((web_image, sigaa_image)):
         for k, kernel in enumerate((kernel1, kernel2)):
@@ -31,11 +31,14 @@ for padding in ('zero', 'mean'):
             for (i, j), x in np.ndenumerate(image):
                  filtered[i, j] = image[i-1:(i+1)+1, j-1:(j+1)+1].dot(kernel).sum()
 
-            filtered = misc.toimage(filtered, cmin=0)
+            filtered = scipy.misc.toimage(filtered, cmin=0)
             filtered.save(padding+str(('_web', '_sigaa')[im])+str(k+1)+'filtered_lena.png')
 
             sc = scipy.ndimage.filters.convolve(image, kernel)
-            sc = misc.toimage(sc, cmin=0)
+            sc = scipy.misc.toimage(sc, cmin=0)
 
-            print(compare_ssim(misc.fromimage(filtered), misc.fromimage(sc)))
+            print(compare_ssim(scipy.misc.fromimage(filtered), scipy.misc.fromimage(sc)))
             sc.save(padding+str(('_web', '_sigaa')[im])+str(k+1)+'filtered_lena2.png')
+
+plt.imshow(filtered, cmap='gray')
+plt.show()
