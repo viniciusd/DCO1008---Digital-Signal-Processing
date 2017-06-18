c5944fd23b63601bb32ec8d065adc704740af5dc
Vinícius Dantas <viniciusd@Viniciuss-MacBook-Pro.local>
2017-06-17 23:36:58 -0300

    Seems to fix idct2 function

--- projeto3/main.py
+++ projeto3/main.py
@@ -9,7 +9,7 @@ threshold = 10**-3
 assert sndhdr.what(filename).filetype == 'aiff'
 
 def dct(x):
-    return fftpack.dct(x, norm='ortho')
+    return fftpack.dct(x, norm='ortho')#.astype(x.dtype)
 
 def dct2(x):
     ret = np.apply_along_axis(dct, 0, x)
@@ -17,24 +17,17 @@ def dct2(x):
     #print(ret)
     return ret
     
-def idct(x):
-    return np.round(fftpack.idct(x, norm='ortho'))
+def idct(X, dtype=None):
+    return np.round(fftpack.idct(X, norm='ortho'))#.astype(X.dtype)
 
-def idct2(X):
-    ret = np.apply_along_axis(idct, 0, X)
-    left = idct(X[:, 0])
-    right = idct(X[:, 1])
-
-    #import pdb; pdb.set_trace()
-    print('IDCT')
-    print('Got:')
-    print(X)
-    print('Returning:')
-    print(ret)
-    return ret
+def idct2(X, dtype=None):
+    if dtype is None:
+        dtype = 'foat'
+    return np.apply_along_axis(idct, 0, X).astype(dtype)
 
 def zeros(arr):
     return np.zeros(arr.shape, dtype=arr.dtype)
+
 x = aifc.open('song.aif')
 
 data = x.readframes(x.getnframes())
@@ -46,15 +39,18 @@ x2[:,0] = dct(x2[:, 0])
 x2[:,1] = dct(x2[:, 1])
 
 y2 = zeros(sig)
-y2[:,0] = idct(dct2(sig)[:, 0])
-y2[:,1] = idct(dct2(sig)[:, 1])
+mydct = dct2(sig)
+#y2[:,0] = idct(mydct[:, 0])
+#y2[:,1] = idct(mydct[:, 1])
 
+y2 = np.apply_along_axis(idct, 0, dct2(sig)).astype(y2.dtype)
 
-y3 = idct2(dct2(sig))
+y3 = zeros(sig)
+y3 = idct2(dct2(sig), dtype=sig.dtype)
 
 y = aifc.open('song2.aif', 'wb')
 y.setnchannels(x.getnchannels())
 y.setsampwidth(x.getsampwidth())
 y.setframerate(x.getframerate())
 #y.writeframes(idct2(X).flatten().tobytes())
-y.writeframes(y2.flatten().tobytes())
+y.writeframes(y3.flatten().tobytes())
