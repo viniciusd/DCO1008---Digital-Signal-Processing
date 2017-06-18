529ac0b72a5bf6c3dbe5294e7489d4ff0f425f7a
Vinícius Dantas <viniciusd@Viniciuss-MacBook-Pro.local>
2017-06-18 11:24:38 -0300

    Adds mutual_information metric

--- projeto3/main.py
+++ projeto3/main.py
@@ -1,12 +1,17 @@
-from scipy import fftpack 
 import aifc
-import numpy as np
 import sndhdr
 
+import numpy as np
+from scipy import fftpack 
+from sklearn.metrics import normalized_mutual_info_score
+
 filename = 'song.aif'
-threshold = 10**-3
+threshold = 2*10**1
 
 assert sndhdr.what(filename).filetype == 'aiff'
+
+def mutual_info(x, y):
+    return normalized_mutual_info_score(x.flatten(), y.flatten())
 
 def dct(x):
     return fftpack.dct(x, norm='ortho')
@@ -23,13 +28,19 @@ def idct2(X, dtype='float'):
 def zeros(arr):
     return np.zeros(arr.shape, dtype=arr.dtype)
 
-x = aifc.open('song.aif')
+x = aifc.open(filename)
 
 data = x.readframes(x.getnframes())
 sig = np.frombuffer(data, dtype='<i2').reshape(-1, x.getnchannels())
+del data
 
-y2 = idct2(dct2(sig), dtype=sig.dtype)
+y2 = dct2(sig)
+y2[np.abs(y2) < threshold] = 0
+y2 = idct2(y2, dtype=sig.dtype)
 
+print(mutual_info(sig, y2))
+
+#import pdb; pdb.set_trace()
 y = aifc.open('song2.aif', 'wb')
 y.setnchannels(x.getnchannels())
 y.setsampwidth(x.getsampwidth())
