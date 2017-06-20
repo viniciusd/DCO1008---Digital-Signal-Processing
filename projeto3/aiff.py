12c8b83aff9c8edfa79a27385da90783b2660938
Vinícius Dantas <viniciusd@Viniciuss-MacBook-Pro.local>
2017-06-19 23:21:14 -0300

    Adds ability to save one channel only

--- projeto3/aiff.py
+++ projeto3/aiff.py
@@ -17,8 +17,15 @@ class Aiff:
         self.sig = utils.from_buffer(data).reshape(-1, x.getnchannels())
 
     def save(self, filename):
-        y = aifc.open('song2.aif', 'wb')
+        y = aifc.open(filename, 'wb')
         y.setnchannels(self.nchannels)
         y.setsampwidth(self.sampwidth)
         y.setframerate(self.framerate)
         y.writeframes(self.sig.flatten().tobytes())
+
+    def save_channel(self, filename, channel):
+        y = aifc.open(filename, 'wb')
+        y.setnchannels(1)
+        y.setsampwidth(self.sampwidth)
+        y.setframerate(self.framerate)
+        y.writeframes(self.sig[:, channel].flatten().tobytes())
